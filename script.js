b619ad388dc481fd3b50a0fcd0d689d8eef10ef4
Karina Krauze <rinafurude@gmail.com>
2025-03-28 20:06:26 +0300

    add animation to header

--- script.js
+++ script.js
@@ -38,4 +38,22 @@ const swiper2 = new Swiper('.swiper2', {
     },
   });
   
+
+  let lastScrollTop = 0;
+  const header = document.querySelector('header');
+  const headerHeight = header.offsetHeight;
+  
+  window.addEventListener('scroll', function() {
+    const scrollTop = window.scrollY;
+    
+    if (scrollTop > lastScrollTop) {
+      // Прокрутка вниз
+      header.classList.add('hidden');
+    } else {
+      // Прокрутка вверх
+      header.classList.remove('hidden');
+    }
+    
+    lastScrollTop = scrollTop;
+  });
   
