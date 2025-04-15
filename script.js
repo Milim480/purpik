1c48f90ee290e5a90e6aeb8635530a80a233b924
Milim480 <128252571+Milim480@users.noreply.github.com>
2025-04-15 14:13:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,4 +17,43 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 100,
+    centeredSlides: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  let lastScrollTop = 0;
+   const header = document.querySelector('header');
+   const headerHeight = header.offsetHeight;
+   
+   window.addEventListener('scroll', function() {
+     const scrollTop = window.scrollY;
+     
+     if (scrollTop > lastScrollTop) {
+       // Прокрутка вниз
+       header.classList.add('hidden');
+     } else {
+       // Прокрутка вверх
+       header.classList.remove('hidden');
+     }
+     
+     lastScrollTop = scrollTop;
+   });
+  
   
